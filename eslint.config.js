7ce1f02881fa0f2fa23a15453f3e1c6ff38e63cc
Max Zviagintsev <maxim.zviagintsev@gmail.com>
2024-08-09 20:53:25 +0300

    Set up eslint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,19 +2,32 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintReact from 'eslint-plugin-react'
 import tseslint from 'typescript-eslint'
+import prettierPlugin from 'eslint-plugin-prettier'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
+  extends: [
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...eslintPluginPrettierRecommended,
+  ],
   files: ['**/*.{ts,tsx}'],
-  ignores: ['dist'],
+  ignores: ['dist', 'node_modules', 'eslint.config.js'],
   languageOptions: {
     ecmaVersion: 2020,
     globals: globals.browser,
+    parserOptions: {
+      project: ['tsconfig.json', 'tsconfig.node.json'],
+    },
   },
   plugins: {
+    '@typescript-eslint': tseslint.plugin,
+    react: eslintReact,
     'react-hooks': reactHooks,
     'react-refresh': reactRefresh,
+    prettier: prettierPlugin,
   },
   rules: {
     ...reactHooks.configs.recommended.rules,
